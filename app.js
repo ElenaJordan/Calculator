6d80ce6839ded1229ad8b6ab529bfb7a9a395062
ElenaJordan <jelliebeanjunse@gmail.com>
2022-03-21 13:45:36 +1000

    added basic boilerplates and basic operator functions

---
+++ app.js
@@ -0,0 +1,21 @@
+//create an Add, Subtract,  Multiply and Divide function
+
+function Add(a, b) {
+    return a + b;
+}
+
+function Subtract(a, b) {
+    return a - b;
+}
+
+function Multiply(a, b) {
+    return a * b;
+}
+
+function Divide(a, b) {
+    return a / b;
+}
+
+function Operate() {
+    
+}
