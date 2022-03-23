ef46c77177b1776ba652f78c71435bcdbaad61cb
ElenaJordan <jelliebeanjunse@gmail.com>
2022-03-23 22:06:10 +1000

    adjusted layout and structure

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//create an Add, Subtract,  Multiply and Divide function
+let displayScreen = document.getElementsByClassName("displayScreen");
 
 function Add(a, b) {
     return a + b;
@@ -16,6 +16,22 @@ function Divide(a, b) {
     return a / b;
 }
 
-function Operate() {
+function Operate(a, b, operator) {
+    if (operator === "plus") {
+        return Add(a, b);
+    } else if (operator === "minus") {
+        return Subtract(a, b);
+    } else if (operator === "times") {
+        return Multiply(a, b);
+    } else {
+        return Divide(a, b);
+    }
+}
+
+function Clear() {
+    document.getElementById("displayScreen").innerHTML = "";
+}
+
+function addDigit(digit) {
     
 }
